e2f496cb9146d71874a3f3a509dd34f2b9d66090
rb-bbw <remo.bergamin@lernende.bbw.ch>
2021-11-12 15:09:23 +0100

    Do task

--- src/ch/bbw/Main.java
+++ src/ch/bbw/Main.java
@@ -3,22 +3,32 @@ package ch.bbw;
 import java.util.Stack;
 
 public class Main {
-    private static Stack<Integer> a = new Stack<>();
-    private static Stack<Integer> b = new Stack<>();
-    private static Stack<Integer> c = new Stack<>();
+    private static Stack<Integer> source = new Stack<>();
+    private static Stack<Integer> help = new Stack<>();
+    private static Stack<Integer> target = new Stack<>();
 
     public static void main(String[] args) {
-        init(5);
-        show();
+        int count = 5;
+        init(count);
+        move(count, source, help, target);
     }
     private static void init(int count){
         for(int i = count; i > 0; i--){
-            a.push(i);
+            source.push(i);
         }
     }
     private static void show() {
-        System.out.println(a.toString());
-        System.out.println(b.toString());
-        System.out.println(c.toString());
+        System.out.println(source.toString());
+        System.out.println(help.toString());
+        System.out.println(target.toString());
+        System.out.println();
+    }
+    private static void move(int count, Stack<Integer> a, Stack<Integer> b, Stack<Integer> c) {
+        if(count > 0){
+            move(count-1, a, c, b);
+            c.push(a.pop());
+            show();
+            move(count-1, b, a, c);
+        }
     }
 }
